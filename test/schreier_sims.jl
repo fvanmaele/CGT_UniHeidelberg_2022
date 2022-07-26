952ac18a4e3be62fab63c9b27b26820a9f2f9724
Ferdinand Vanmaele <Vanmaele@stud.uni-heidelberg.de>
2022-07-26 17:07:11 +0200

    schreier_sims.jl: correct depth check in push!
    Fixes #16

--- test/schreier_sims.jl
+++ test/schreier_sims.jl
@@ -3,6 +3,14 @@
         prod(I∘length, CGT.transversals(sc))
     order(sc::CGT.StabilizerChain) = order(BigInt, sc)
 
+    # Make sure the order of generators does not matter
+    S1 = [perm"(1,2)", perm"(1,2,3)"]
+    sc1 = CGT.schreier_sims(S1)
+    S2 = [perm"(1,2,3)", perm"(1,2)"]
+    sc2 = CGT.schreier_sims(S2)
+    @test order(sc1) == order(sc2)
+    @test order(sc1) == 6
+
     for group_order in 2:30
         for S in SmallPermGroups[group_order]
             sc = CGT.schreier_sims(S) # defaults to Transversal
